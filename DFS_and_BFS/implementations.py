3fde92073820341df3c1bb0ead3fe275575c7a81
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-13 14:54:25 -0700

    Create implementations.py

---
+++ DFS_and_BFS/implementations.py
@@ -0,0 +1,29 @@
+# IMPLEMENTATION OF DFS (for reference)
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+
+# IMPLEMENTATION OF BFS (for reference)
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
